9476f81345a3773a1128c31f8fd8290c2d56d0ea
etokiniaina <e.tokiniaina@gmail.com>
2025-04-26 19:21:50 +0200

    feat: add screescreenshots and demo_main

--- lib/models/collection.dart
+++ lib/models/collection.dart
@@ -6,13 +6,11 @@ import 'product.dart';
 
 class Collection extends Equatable {
   final String name;
-  final int? counts;
   final List<Product> products;
   final DateTime createdAt;
 
   const Collection({
     required this.name,
-   this.counts,
     this.products = const [],
     required this.createdAt,
   });
@@ -21,7 +19,6 @@ class Collection extends Equatable {
   Map<String, dynamic> toMap() {
     return {
       'name': name,
-      'counts': counts,
       'products': products,
       'created_at': createdAt,
     };
@@ -30,7 +27,6 @@ class Collection extends Equatable {
   factory Collection.fromMap(Map<String, dynamic> json) {
     return Collection(
       name: json['name'] as String,
-      counts: json['counts'] as int?,
       products: json['products'] != null ? List<Product>.from(json['products'].map((x) => Product.fromMap(x))) : [],
       createdAt: DateTime.parse(json['created_at']),
     );
@@ -39,20 +35,17 @@ class Collection extends Equatable {
   @override
   List<Object?> get props => [
     name,
-    counts,
     products,
     createdAt,
   ];
 
   Collection copyWith({
     String? name,
-    int? counts,
     List<Product>? products,
     DateTime? createdAt,
   }) {
     return Collection(
       name: name ?? this.name,
-      counts: counts ?? this.counts,
       products: products ?? this.products,
       createdAt: createdAt ?? this.createdAt,
     );
